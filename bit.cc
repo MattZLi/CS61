5911b24dc850e3463bb2ff5bac8f800446f66ac5
Justin Zhu <justinthezhu@gmail.com>
2018-10-16 16:29:52 -0400

    Saved new changes 2018-10-16 16:29:52

--- bit.cc
+++ bit.cc
@@ -1,6 +1,30 @@
 #include<stdio.h>
-int main(){
-  int x = ~2;
-  printf("%i\n",x);
+#include<climits>
+//assumes little endian
+void printBits(size_t const size, void const * const ptr)
+{
+    unsigned char *b = (unsigned char*) ptr;
+    unsigned char byte;
+    int i, j;
+
+    for (i=size-1;i>=0;i--)
+    {
+        for (j=7;j>=0;j--)
+        {
+            byte = (b[i] >> j) & 1;
+            printf("%u", byte);
+        }
+    }
+    puts("");
 }
-  
+
+int main(int argv, char* argc[])
+{
+        int i = ~2;
+        unsigned int ui = UINT_MAX;
+        float f = 23.45f;
+        printBits(sizeof(i), &i);
+        printBits(sizeof(ui), &ui);
+        printBits(sizeof(f), &f);
+        return 0;
+}
